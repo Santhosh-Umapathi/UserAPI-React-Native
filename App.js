bf091ca10869bd8538c01efaee71f33e2b024525
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-02 10:46:05 +0200

    Completed User API

--- App.js
+++ App.js
@@ -1,19 +1,111 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, TouchableOpacity, Button, TextInput, ActivityIndicator, Image, FlatList } from 'react-native';
+import {Card, CardItem} from 'native-base'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class HomeScreen extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      dataSource: []
+    }
+  }
+
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+
+
+
+  getAllUsers = () => {
+    return (
+      fetch("https://randomuser.me/api/?results=50") 
+        .then(response => response.json())
+        .then(responseJSON => {
+          this.setState({
+            isLoading: false,
+            dataSource: this.state.dataSource.concat(responseJSON.results)
+          })
+          console.log(this.state.dataSource)
+        })
+      .catch((error) => {console.log(error)})
+    )
+  }
+  
+
+
+
+
+
+
+
+  render() {
+    if (this.state.isLoading === true)
+    {
+      return (
+        <View style={ styles.containerView}>
+            <ActivityIndicator size="large" color="lightgreen" />
+        </View>
+      );
+    }
+    else {
+      return (
+        <View style={styles.containerView}>
+          <Text style={{fontSize: 50, alignSelf:'center', marginBottom:10}}> User API </Text>
+          <FlatList
+            data={this.state.dataSource}
+            keyExtractor={item => item.email}
+            renderItem={({ item }) => (
+              <Card>
+                <CardItem>
+                  <View style={{alignItems:'stretch'}}>
+                    <Image
+                      source={{ uri: item.picture.medium }}
+                      style={styles.profilepic}
+                    />
+                  </View>
+                </CardItem>
+                <CardItem>
+                  <View style={styles.userInfo}>
+                    <Text style={{fontSize: 25}}>
+                      {item.name.first} {item.name.last}
+                    </Text>
+                    <Text style={{fontSize: 18, color:'gray'}}>
+                      {item.email} ,{item.phone}
+                    </Text>
+                  </View>
+                </CardItem>
+              </Card>
+            )}
+          />
+        </View>
+      );
+    }}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  
+const styles = StyleSheet.create(
+{
+    containerView:
+    {
+      flex: 1,
     justifyContent: 'center',
-  },
+      marginTop: 80
+      //alignItems:"center"
+    },
+    profilepic:
+    {
+      flex:2,
+      height: 100,
+      width: 100,
+      marginEnd: 10,
+      borderRadius: 100
+    },
+    userInfo:
+    {
+      flex: 5,
+      flexDirection: 'column'
+    }
+  
 });
